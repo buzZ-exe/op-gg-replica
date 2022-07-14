370387c0ca8bda7ecabf7e35118a4df0601ad793
Kevin George <kevingeorge379@gmail.com>
2022-07-14 18:00:33 +0400

    Mousemove parallax WIP

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-var form = document.querySelector("form");
+// redirect the search to actual op.gg
+const form = document.querySelector("form");
 
 form.addEventListener("submit", function (e) {
   e.preventDefault();
-  var search = form.querySelector("input[type=search]");
+  const search = form.querySelector("input[type=search]");
   search.value = "https://euw.op.gg/summoners/euw/" + search.value;
   window.location.replace(search.value);
 });
+
+
+// move bg with mouse (not working)
+// const element = document.querySelector("div.hover");
+// const button = document.querySelector("body");
+
+// element.addEventListener("mousemove", (e) => {
+//   body.style.backgroundPositionX = -e.offsetX/100 + "px";
+//   body.style.backgroundPositionY = -e.offsetY/100 + "px";
+// });
